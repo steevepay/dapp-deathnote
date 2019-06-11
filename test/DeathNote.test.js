5fab6f0cb22a86ca49e57f1e2ba997593f67359d
Steeve Payraudeau <steeve.payraudeau@epitech.eu>
2019-06-11 20:24:17 +0400

    New test passed create a new death

--- test/DeathNote.test.js
+++ test/DeathNote.test.js
@@ -26,8 +26,22 @@ contract("DeathNote", (accounts) => {
 
   it("create a new death", async () => {
     const deathnote = await Deathnote.deployed();
-    await deathnote.addDeath('John', 'heart attack', '2019/10/18', 'base64://');
-    // compare the length of dead at 1
-    // get pushed death and compage the name deasc date and image
+    const name = 'John';
+    const conditions = 'heart attack';
+    const date = '2019/10/18';
+    const img = 'base64://'
+    await deathnote.addDeath(name, conditions, date, img, {
+      value: web3.utils.toWei('0.001', 'ether')
+    });
+    const nbrdeaths = await deathnote.getDeathsLength();
+    assert.equal(nbrdeaths, 1);
+    const death = await deathnote.deaths(0);
+    assert.ok(death);
+    assert.equal(name, death.name);
+    assert.equal(conditions, death.conditions);
+    assert.equal(date, death.timeOfDeath);
+    assert.equal(img, death.img);
   });
+
+
 })
