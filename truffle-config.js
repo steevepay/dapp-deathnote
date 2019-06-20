d95afe25313e5bfdc87ced3d765ac677196b7313
Steeve Payraudeau <steeve.payraudeau@epitech.eu>
2019-06-20 16:19:23 +0400

    Contract in main network

--- truffle-config.js
+++ truffle-config.js
@@ -81,11 +81,10 @@ module.exports = {
           mnemonic,
           `https://mainnet.infura.io/v3/aa7a461490244b65a54e2f67b959fa8a`
         ),
-      network_id: 1, // Ropsten's id
-      gas: 5500000, // Ropsten has a lower block limit than mainnet
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+      network_id: 1,
+      gas: 5500000,
+      gasPrice: 5000000000,
+      confirmations: 2
     }
 
     // Useful for private networks
